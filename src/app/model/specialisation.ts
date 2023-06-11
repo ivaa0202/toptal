8b814ac3f8aa8d29f790c4fe8f5f32566cb704ce
ivaa0202 <ivaa.0202@gmail.com>
2023-06-11 20:46:50 +0200

    Auto-generated commit [ci skip]

--- src/app/model/specialisation.ts
+++
@@ -1,9 +0,0 @@
-export class Specialisation {
-    id: number;
-    title: string;
-
-    constructor(obj?: any) {
-        this.id = obj && obj.id || 0;
-        this.title = (obj && obj.title) || '';
-    }
-}
